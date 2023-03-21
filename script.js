f48188f5cdcdb22387c2fa25952407cc04ad0121
matheus-fnprado <matheusfilipe573@gmail.com>
2023-03-21 19:50:34 -0300

    publicação

---
+++ script.js
@@ -0,0 +1,54 @@
+const inputPergunta = document.getElementById("inputPergunta");
+const inputPergunta_1 = document.getElementById("inputPergunta_1");
+const inputPergunta_2 = document.getElementById("inputPergunta_2");
+const resultadoIA = document.getElementById("resultadoIA");
+
+const OPENAI_API_KEY = "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"
+
+function EnviarPergunta() {
+	var valorPergunta = 'Como melhorar a ' + inputPergunta.value + ' do meu e-commerce que hoje é de ' + inputPergunta_1.value + '. A minha meta é alcançar uma melhora de ' + inputPergunta_2.value + ' nessa métrica. Lembre-se de apresentar uma resposta curta e com ações de efeito rápido';
+
+  fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json", 
+      Authorization: "Bearer " + OPENAI_API_KEY,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: valorPergunta,
+      max_tokens: 2048, // tamanho da resposta
+      temperature: 0.8, // criatividade na resposta
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      if (resultadoIA.value) resultadoIA.value += "\n";
+
+      if(json.error?.message){
+        resultadoIA.value += `Error: ${json.error.message}`;
+      } else if (json.choices?.[0].text) {
+        var text = json.choices[0].text || "Sem resposta";
+
+        resultadoIA.value += "Chat GPT: " + text;
+      }
+
+      resultadoIA.scrollTop = resultadoIA.scrollHeight;
+    })
+    .catch((error) => console.error("Error:", error))
+    .finally(() => {
+      inputPergunta.value = "";
+      inputPergunta.disabled = false;
+      inputPergunta.focus();
+    });
+
+	if (resultadoIA.value) resultadoIA.value += "\n\n\n";
+
+	inputPergunta.value = "Aguarde enquanto geramos a sua estratégia...";
+	inputPergunta.disabled = true;
+
+  resultadoIA.scrollTop = resultadoIA.scrollHeight;
+}
+
+document.getElementById("EnviarPergunta").addEventListener("click", EnviarPergunta)
